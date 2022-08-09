dae67c8a417f3cde57c039d253d7c51bbcde7060
Jennifer Ong <jennifer.ex.ong@gmail.com>
2022-08-09 21:22:15 +1200

    Set up theme type

---
+++ src/styles/mixins/fontMixin.ts
@@ -0,0 +1,14 @@
+import { css, FlattenSimpleInterpolation } from 'styled-components';
+import { Font } from 'styles/types';
+
+export const fontMixin = (font: Font): FlattenSimpleInterpolation => {
+  const { family, baseSize, baseWeight, color, lineHeight } = font;
+
+  return css`
+    font-family: ${family};
+    font-size: ${baseSize};
+    font-weight: ${baseWeight}
+    color: ${color};
+    line-height: ${lineHeight};
+  `;
+};
