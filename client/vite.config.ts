269a917ebee449c3bda2a5ad3fe7fda4005d0023
Jennifer Ong <jennifer.ex.ong@gmail.com>
2022-11-19 19:17:33 +1300

    Move front-end into client directory

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigpaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigpaths(), svgr()],
+  plugins: [
+    react(),
+    tsconfigpaths(),
+    svgr(),
+    VitePWA({ registerType: 'autoUpdate' }),
+  ],
 });
