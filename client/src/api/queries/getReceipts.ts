c710f4996c31618cd4fcc57aace5cb66eee1437b
Jennifer Ong <jennifer.ex.ong@gmail.com>
2023-01-15 01:33:26 +1300

    Update getReceipts to return required data

--- client/src/api/queries/getReceipts.ts
+++ client/src/api/queries/getReceipts.ts
@@ -2,11 +2,39 @@ import { ApiReceipt } from 'api';
 import { gql, useClient } from 'urql';
 import { QueryResult, ReceiptsResult } from './types';
 
-// TODO
 const query = gql`
   query {
     receipts {
       id
+      date
+      store {
+        id
+        name
+      }
+      paidBy {
+        id
+        firstName
+        lastName
+        email
+      }
+      receiptLines {
+        id
+        product {
+          id
+          name
+        }
+        price
+        splits {
+          id
+          person {
+            id
+            firstName
+            lastName
+            email
+          }
+          amount
+        }
+      }
     }
   }
 `;
@@ -23,6 +51,11 @@ export const useGetReceipts = (): QueryResult<ApiReceipt[]> => {
     if (error || !data) {
       throw new Error(JSON.stringify(error));
     }
+
+    // convert date strings to Date
+    data.receipts.forEach(receipt => {
+      receipt.date = new Date(receipt.date);
+    });
 
     return data.receipts;
   };
