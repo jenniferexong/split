7c1ecf263356c5a03d8c75440a4109dc6ee717b2
Jennifer Ong <jennifer.ex.ong@gmail.com>
2023-01-15 16:04:18 +1300

    Use ReceiptTitleRow component in entry Invoice

--- client/src/components/entry/Invoice.tsx
+++ client/src/components/entry/Invoice.tsx
@@ -1,5 +1,6 @@
 import { InvoiceData } from 'calculator/types';
 import { Paper } from 'components/board';
+import { ReceiptTitleRow } from 'components/receipt';
 import { Table, TableCell, TableRow } from 'components/table';
 import { useTheme } from 'styled-components';
 import { ReceiptContainer } from '../receipt/ReceiptContainer';
@@ -15,31 +16,27 @@ export const Invoice = (props: InvoiceProps) => {
     <Paper width={theme.components.invoice.width}>
       <ReceiptContainer>
         <Table>
-          <thead>
-            <TableRow borderBottom>
-              <TableCell as="th" textAlign="center" colSpan={2}>
-                Invoice
-              </TableCell>
-            </TableRow>
-          </thead>
+          <ReceiptTitleRow>Invoice</ReceiptTitleRow>
           <tbody>
             <TableRow borderTop>
-              <TableCell>Total</TableCell>
-              <TableCell textAlign="right">
+              <TableCell colSpan={2}>Total</TableCell>
+              <TableCell colSpan={2} textAlign="right">
                 ${totalSpendings.toFixed(2)}
               </TableCell>
             </TableRow>
             <TableRow borderBottom>
-              <TableCell>
+              <TableCell colSpan={2}>
                 <h5>Actual</h5>
               </TableCell>
-              <TableCell textAlign="right">
+              <TableCell colSpan={2} textAlign="right">
                 <h5>${actualSpendings.toFixed(2)}</h5>
               </TableCell>
             </TableRow>
             <TableRow borderTop>
-              <TableCell>Oweings</TableCell>
-              <TableCell textAlign="right">${oweings.toFixed(2)}</TableCell>
+              <TableCell colSpan={2}>Oweings</TableCell>
+              <TableCell colSpan={2} textAlign="right">
+                ${oweings.toFixed(2)}
+              </TableCell>
             </TableRow>
           </tbody>
         </Table>
